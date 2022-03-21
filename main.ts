f2ad4ceb7bff6d5908a941a5cc4e5d3db4196cbf
Heitor0 <101339934+Heitor0@users.noreply.github.com>
2022-03-21 09:51:53 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(0 + 1)
+    basic.showNumber(1 + 1)
+    basic.showNumber(1 + 2)
+    basic.showNumber(1 + 3)
+    basic.showIcon(IconNames.No)
+    music.playTone(294, music.beat(BeatFraction.Double))
+    basic.showIcon(IconNames.No)
+    music.playTone(294, music.beat(BeatFraction.Double))
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
