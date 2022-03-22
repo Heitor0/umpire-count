8c1c0a088eb9312d592ef3240bd4869a8b3c5039
Heitor0 <101339934+Heitor0@users.noreply.github.com>
2022-03-22 06:17:07 -0700

    fully finished update

--- main.ts
+++ main.ts
@@ -1,15 +1,31 @@
+let Number_of_Balls = 0
+let num_strikes = 0
 input.onButtonPressed(Button.A, function () {
-    basic.showNumber(0 + 1)
-    basic.showNumber(1 + 1)
-    basic.showNumber(1 + 2)
-    basic.showNumber(1 + 3)
-    basic.showIcon(IconNames.No)
-    music.playTone(294, music.beat(BeatFraction.Double))
-    basic.showIcon(IconNames.No)
-    music.playTone(294, music.beat(BeatFraction.Double))
+    Number_of_Balls += 1
+    basic.showNumber(Number_of_Balls)
+    if (Number_of_Balls == 4) {
+        basic.showIcon(IconNames.No)
+        music.playTone(294, music.beat(BeatFraction.Double))
+        basic.showIcon(IconNames.No)
+        music.playTone(294, music.beat(BeatFraction.Double))
+        basic.clearScreen()
+        Number_of_Balls = 0
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    control.reset()
 })
 input.onButtonPressed(Button.B, function () {
-	
+    num_strikes += 1
+    basic.showNumber(num_strikes)
+    if (num_strikes == 3) {
+        basic.showIcon(IconNames.No)
+        music.playTone(294, music.beat(BeatFraction.Double))
+        basic.showIcon(IconNames.No)
+        music.playTone(294, music.beat(BeatFraction.Double))
+        basic.clearScreen()
+        num_strikes = 0
+    }
 })
 basic.forever(function () {
 	
